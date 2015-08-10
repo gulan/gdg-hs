bc4cba00e537f73616e93e480fc702da81b7dcd1
gulan <glen.wilder@gmail.com>
2015-08-09 20:20:31 -0700

    Use infix </> instead of combine.

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Control.Monad (when)
 import System.Directory (createDirectory, doesDirectoryExist,
                          getCurrentDirectory, getDirectoryContents, removeFile)
 import System.Environment (getArgs)
-import System.FilePath (combine,takeFileName)
+import System.FilePath ((</>),takeFileName)
 import System.FilePath.Glob (glob)
 import System.Posix.Files (createSymbolicLink)
 
@@ -13,7 +13,7 @@ zfill n = let ns = show n in replicate (3-length ns) '0' ++ ns
 
 listGenerations :: FilePath -> IO [Int]
 listGenerations genname = do
-  paths <- glob $ combine genname "[0-9][0-9][0-9]"
+  paths <- glob $ genname </> "[0-9][0-9][0-9]"
   let gens = map (read . takeFileName) paths  :: [Int]
   return gens
 
@@ -24,7 +24,7 @@ lastGeneration genname = do
 
 newSymbolicLink :: FilePath -> FilePath -> FilePath -> IO ()
 newSymbolicLink gdgName target linkName = do
-  let symlink = combine gdgName linkName
+  let symlink = gdgName </> linkName
   ps <- getDirectoryContents gdgName
   when (elem linkName ps) $ do
     removeFile symlink
@@ -32,13 +32,13 @@ newSymbolicLink gdgName target linkName = do
 
 makeGeneration :: FilePath -> Int -> IO String
 makeGeneration gdgName gen = do
-  let newgen = combine gdgName $ zfill gen
+  let newgen = gdgName </> (zfill gen)
   createDirectory newgen
   newSymbolicLink gdgName (zfill gen) "latest"
   when (gen > 1) $ do
     newSymbolicLink gdgName (zfill (gen-1)) "previous"
   cwd <- getCurrentDirectory
-  return $ combine cwd newgen
+  return $ cwd </> newgen
   
 main = do
   [gdgName] <- getArgs
